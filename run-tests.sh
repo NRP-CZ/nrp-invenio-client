baa9888fb49c11bd158af7d1b59032411ec05625
Mirek Simek <miroslav.simek@gmail.com>
2024-03-14 20:49:21 +0100

    Added github workflow

---
+++ run-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo ""
+echo ""
+echo "======================================"
+echo ""
+echo ""
+echo "Run tests is not implemented yet !!!!"
+echo ""
+echo ""
+echo "======================================"
+echo ""
+echo ""
